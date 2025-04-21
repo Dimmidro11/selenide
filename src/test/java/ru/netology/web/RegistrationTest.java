64f5b938b60569ca0eeb457028a39d229e448395
Dimmidroll <diman15061997@mail.ru>
2025-04-21 22:20:14 +0500

    quest 1, 2 done

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,34 +1,59 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.security.Key;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class RegistrationTest {
+
+    public String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
     @Test
     void shouldRegisterByAccountNumberDOMModification() {
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+
         open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $("[name='number']").setValue("4055 0100 0123 4613 8564");
-        $("[name='phone']").setValue("+792000000000");
-        $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofMillis(5000));
-        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
+        SelenideElement formElement = $("form");
+        formElement.$("[data-test-id='city'] input").setValue("Екатеринбург");
+        formElement.$("[data-test-id='date'] input.input__control").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        formElement.$("[data-test-id='date'] input.input__control").setValue(planningDate);
+        formElement.$("[data-test-id='name'] input").setValue("Иванов Иван");
+        formElement.$("[data-test-id='phone'] input").setValue("+79600000000");
+        formElement.$("[data-test-id='agreement']").click();
+        formElement.$(".button").click();
+        $("[data-test-id='notification'] .notification__content").should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно забронирована на " + planningDate));
     }
 
     @Test
     void shouldRegisterByAccountNumberVisibilityChange() {
+        String planningDate = generateDate(7, "d");
+
         open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $$("[name='number']").last().setValue("4055 0100 0123 4613 8564");
-        $$("[name='phone']").last().setValue("+792000000000");
-        $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofSeconds(5));
-        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(5));
+        SelenideElement formElement = $("form");
+        formElement.$("[data-test-id='city'] input").setValue("Ек");
+        $(Selectors.withText("Екатеринбург")).click();
+        formElement.$(".icon-button").click();
+        $(Selectors.withText(planningDate)).click();
+        formElement.$("[data-test-id='name'] input").setValue("Иванов Иван");
+        formElement.$("[data-test-id='phone'] input").setValue("+79600000000");
+        formElement.$("[data-test-id='agreement']").click();
+        formElement.$(".button").click();
+        $("[data-test-id='notification'] .notification__content").should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно забронирована на " + planningDate));
     }
 }
 
